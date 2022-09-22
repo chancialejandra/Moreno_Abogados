36ed80d4583737d2c6ceef16b46e05728a848b4f
JPosada <wondergfulomg@gmail.com>
2022-09-22 18:21:25 -0500

    Cambio en la conexion a bd

--- model/conexion.php
+++ model/conexion.php
@@ -8,7 +8,7 @@ class ConexionBD
 
   static public function conectar()
   {
-    $abogado = new PDO("mysql:host=ble5mmo2o5v9oouq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306; dbname=g1f0bqgri51p1rbg", "nnmhmwoli5741v3f", "h46cy7dzz9ff2yk9");
+    $abogado = new PDO("");
     $abogado -> exec("set names utf8");
     return $abogado;
   }
