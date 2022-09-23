06ddcf86e5703fee9a4a6ea2b75ba2ca18e4f7af
JPosada <wondergfulomg@gmail.com>
2022-09-23 16:57:48 -0500

    conexion bd equipo juanma

--- model/conexion.php
+++ model/conexion.php
@@ -8,7 +8,7 @@ class ConexionBD
 
   static public function conectar()
   {
-    $abogado = new PDO("");
+    $abogado = new PDO("mysql:host=localhost; dbname=bd_moreno_abogados", "root", "");
     $abogado -> exec("set names utf8");
     return $abogado;
   }
